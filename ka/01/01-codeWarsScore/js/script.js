1e90f17b730428da633938e233969b05d078bd45
2020b Gavlik Martin <144203996+b0gavmar@users.noreply.github.com>
2024-09-19 12:30:22 +0200

    Add files via upload

--- ka/01/01-codeWarsScore/js/script.js
+++ ka/01/01-codeWarsScore/js/script.js
@@ -23,7 +23,7 @@ function FetchScoreSum() {
                 let li = '<div class="container-2"><h3>Keresett felhasználó: ' + json.username + '</h3>';
 
                 li += 'Név: ' + json.name + '<br> Honor: ' + json.honor + '<br>'
-                    + 'Klán:' + json.clan + '<br> Dobogós helyezés:' + json.leaderboardPosition
+                    + 'Klán: ' + json.clan + '<br> Dobogós helyezés: ' + json.leaderboardPosition
                     + '<br>Rank: ' + json.ranks.overall.name + '(' + json.ranks.overall.rank + ',' + json.ranks.overall.color + ')' + '<br> Pontszám: ' + json.totalPoints + '</div>';
                 let li2 = li.replaceAll(null, "---");
                 let li3 = li2.replaceAll(NaN, "---");
@@ -44,7 +44,7 @@ function FetchScoreSum() {
 
 function FetchScoreByLanguages() {
     if (userName) {
-        fetch("https://www.codewars.com/api/v1/users/" + userName + "/languages")
+        fetch("https://www.codewars.com/api/v1/users/" + userName)
             .then(response => {
                 if (!response.ok) {
                     throw new Error("Error 404 - A keresett felhasználó nem található");
@@ -54,16 +54,31 @@ function FetchScoreByLanguages() {
                 }
             })
             .then(json => {
-                console.log(json);
+                console.table(json.ranks.languages);
                 let li = '<div class="container-2"><h3>Keresett felhasználó: ' + json.username + '</h3>';
-                json.forEach(rank => {
-                    li += element.language + ':' + element.rank + '<br>';
-                });
+                const languageRanks = json.ranks.languages;
+                for(let language in languageRanks){
+                    li += language + ':' + languageRanks[language].rank + '<br>';
+                }
+                /*json.ranks.languages.forEach(language => {
+                    li += language + ':' + language.rank + '<br>';
+                });*/
                 li += "</div>";
+
+                let li2 = li.replaceAll(null, "---");
+                let li3 = li2.replaceAll(NaN, "---");
+
+                //console.log(li.replaceAll(null||NaN, "---"));
+                console.log(li3);
+
+                document.getElementById("kiiras").innerHTML = li3.replaceAll(undefined, "---") + "</div>";
             }
             ).catch(error => {
                 console.error('Error:', error);
                 document.getElementById("kiiras").innerHTML = "<div class='container-2'>" + error.message + "</div>";
             });
+    }
+    else {
+        document.getElementById("kiiras").innerHTML = "<div class='container-2'>Írjon be egy felhasználó nevet!</div>";
     }
 }
