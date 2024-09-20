2a699d7ee434b43c524f0d74498eb67711368346
2020b Gavlik Martin <144203996+b0gavmar@users.noreply.github.com>
2024-09-20 09:37:03 +0200

    Add files via upload

--- br/01/02-urlap/js.js
+++ br/01/02-urlap/js.js
@@ -1,5 +1,6 @@
 let emailPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 let pwdPattern = /^[\w]{8,256}$/;
+let errorMsg;
 
 //mo irszamok 1000-9999
 //regex101 oldal
@@ -9,18 +10,21 @@ $(function () { //ready
         event.preventDefault();
     });
     $("input").change(function () {
+        errorMsg = "";
         let email = $(this).val();
         let type = $(this).attr("type");
         let pw = $(this).val();
         if (!emailPattern.test(email) && type == "email") {
-            $(".error").html("<p>Az email formátuma nem megfelelő!</p>");
+            errorMsg += "<p>Az email formátuma nem megfelelő!</p>";
+            $(".error").html(errorMsg);
         }
         else {
             $(".error").html("");
         }
 
         if (!pwdPattern.test(pw)) {
-            $(".error").html("<p>Az jelszó formátuma nem megfelelő!</p>");
+            errorMsg += "<p>Az jelszó formátuma nem megfelelő!</p>";
+            $(".error").html(errorMsg);
         }
         else {
             $(".error").html("");
