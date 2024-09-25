6f63f0587147b311f5251052567abfe6a3650a19
2020b Gavlik Martin <144203996+b0gavmar@users.noreply.github.com>
2024-09-25 11:33:53 +0200

    Add files via upload

--- br/01/02-urlap/js.js
+++ br/01/02-urlap/js.js
@@ -1,6 +1,8 @@
 let emailPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 let pwdPattern = /^[\w]{8,256}$/;
-let errorMsg;
+let irszPattern = /^[1-9][4]$/;
+let errors = [];
+let errors2 = [];
 
 //mo irszamok 1000-9999
 //regex101 oldal
@@ -10,25 +12,30 @@ $(function () { //ready
         event.preventDefault();
     });
     $("input").change(function () {
-        errorMsg = "";
-        let email = $(this).val();
+        let email = $(this).val().trim();
+        let pwd = $(this).val().trim();
+        let irsz = $(this).val().trim();
         let type = $(this).attr("type");
-        let pw = $(this).val();
+        
+
         if (!emailPattern.test(email) && type == "email") {
-            errorMsg += "<p>Az email formátuma nem megfelelő!</p>";
-            $(".error").html(errorMsg);
+            errors[1] = "<p>Az email formátuma nem megfelelő!</p>";
         }
-        else {
-            $(".error").html("");
+        else{
+            errors2 = errors.filter(error => !error.includes("email"));
         }
 
-        if (!pwdPattern.test(pw)) {
-            errorMsg += "<p>Az jelszó formátuma nem megfelelő!</p>";
-            $(".error").html(errorMsg);
+        if (!pwdPattern.test(pwd) && type == "password") {
+            errors[2] = "<p>Az jelszó formátuma nem megfelelő!</p>";
         }
-        else {
-            $(".error").html("");
+
+        if (!irszPattern.test(irsz) && type == "text") {
+            errors[3] = "<p>Az irányítószám formátuma nem megfelelő!</p>";
         }
+
+        console.table(errors);
+        console.table(errors2);
+        $(".error").html(errors.join(""));
     });
-    $(".button").attr("disabled",false);
+    $(".button").attr("disabled", false);
 });
